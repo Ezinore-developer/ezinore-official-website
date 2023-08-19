81d485532147a77739817b6c86446687597dc42c
GM-11 <gmathur1001@gmail.com>
2023-08-19 15:18:04 +0530

    removed dead code

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-// import adapter from "@sveltejs/adapter-vercel";
-import adapter from '@sveltejs/adapter-static';
+import adapter from "@sveltejs/adapter-vercel";
+
 import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -14,9 +14,8 @@ const config = {
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter({
-      fallback : "index.html"
+      runtime: "nodejs18.x",
     }),
-    // prerender : true
   },
 };
 
