1673c94682f3645e353102ad66568aae20d8b2cd
GM-11 <gmathur1001@gmail.com>
2023-08-19 14:33:21 +0530

    changed adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from "@sveltejs/adapter-vercel";
+// import adapter from "@sveltejs/adapter-vercel";
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -13,7 +14,8 @@ const config = {
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter({
-      runtime: "nodejs18.x",
+      // runtime: "nodejs18.x",
+      fallback: "index.html"
     }),
   },
 };
